37357bfbd288d4cc8e6b81c2d484f2f69b3a01f3
xtenzanx <xtenzanxt1@gmail.com>
2021-09-07 19:02:30 +0800

    新增範例程式 (完成新增備忘錄功能)

---
+++ ReactIdentityTest/ClientApp/src/pages/Notebook/components/Item.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+
+const Item = ({ note, date, time }) => {
+    return <div className="item">
+        <span>{note}</span>
+        <span>{`${date} ${time}`}</span>
+        <button className="remove">刪除</button>
+    </div>
+}
+
+export default Item
---
+++ ReactIdentityTest/ClientApp/src/pages/Notebook/index.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import { useState } from 'react'
+
+import Edit from './components/Edit'
+import List from './components/List'
+import './index.css'
+
+const Notebook = () => {
+
+    const [data, setData] = useState([])
+
+    return <div className="app">
+        <Edit add={setData}/>
+        <List listData={data} />
+    </div>
+}
+
+export default Notebook
