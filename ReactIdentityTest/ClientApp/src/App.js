5a64b5b0cef300f18d5562accfa7d6caa4afdee4
xtenzanx <xtenzanxt1@gmail.com>
2021-09-08 08:57:59 +0800

    將範例切割為獨立頁面

--- ReactIdentityTest/ClientApp/src/App.js
+++ ReactIdentityTest/ClientApp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
+import Notebook from './pages/Notebook'
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
@@ -17,6 +18,7 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
+        <Route path='/notebook' component={Notebook} />
         <Route path='/counter' component={Counter} />
         <AuthorizeRoute path='/fetch-data' component={FetchData} />
         <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
--- ReactIdentityTest/ClientApp/src/index.js
+++ ReactIdentityTest/ClientApp/src/index.js
@@ -5,15 +5,12 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 //import registerServiceWorker from './registerServiceWorker';
 
-import Notebook from './pages/Notebook'
-
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
     <App />
-    <Notebook />
   </BrowserRouter>,
   rootElement);
 
